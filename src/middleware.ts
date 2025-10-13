745c1acf404c3cc11c8fc2de5574c1d40eb60c94
bist <bist220@gmail.com>
2025-10-13 17:45:49 +0530

    fix middleware paths

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,22 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { verifyToken } from './app/lib/auth';
 
-const PUBLIC_PATHS = ['/', '/login', '/register', '/api/auth', '/api/properties', '/property/', '/api/upload'];
+// const PUBLIC_PATHS = ['/', '/login', '/register', '/api/auth', '/api/properties', '/property/', '/api/upload'];
 
 export async function middleware(req: NextRequest) {
-  const { pathname } = req.nextUrl;
+  // const { pathname } = req.nextUrl;
 
   // Allow public routes
-  if (PUBLIC_PATHS.some(p => pathname.startsWith(p))) return NextResponse.next();
+  // if (PUBLIC_PATHS.some(p => pathname.startsWith(p))) return NextResponse.next();
 
   const token = req.cookies.get('token')?.value;
   if (!token) return NextResponse.redirect(new URL('/login', req.url));
-  console.log(`===>>> middleware :: token ::  ${token}`)
+  
   try {
     const payload = await verifyToken(token);
     if(!payload) {
       throw new Error("Invalid Token");
     }
-    // await jwtVerify(token, new TextEncoder().encode(process.env.JWT_SECRET));
     return NextResponse.next();
   } catch {
     return NextResponse.redirect(new URL('/login', req.url));
@@ -32,7 +31,8 @@ export const config = {
   matcher: [
     '/property/new',
     '/property/edit/:path*',
-    '/api/properties/:path*',
+    // '/api/properties/:path*',
+    '/api/properties/:path/edit',
     '/api/upload',
     '/logout',
   ],
